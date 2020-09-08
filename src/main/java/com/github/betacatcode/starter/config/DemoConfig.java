ca0014f0c1fda9ae2fd001ca95e3765bcabd07f1
wangxiao <1595990651@qq.com>
2020-09-08 17:25:56 +0800

    配置修改

--- src/main/java/com/github/betacatcode/starter/config/DemoConfig.java
+++ src/main/java/com/github/betacatcode/starter/config/DemoConfig.java
@@ -8,11 +8,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @EnableConfigurationProperties可以把相同前缀的配置信息通过配置项名称映射成实体类
- * 这样，我们就能将以demo为前缀的配置项拿到了
- */
-
 @Configuration
 @EnableConfigurationProperties(DemoProperties.class)
 @ConditionalOnProperty(
